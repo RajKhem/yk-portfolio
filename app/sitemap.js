010b87d8a40b2380370106f502cb175dcd97e0f8
Yatri <khemrajpaneru6@gmail.com>
2026-08-10 16:08:20 +0545

    Add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://khemrajpaneru.com.np",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ];
+}
